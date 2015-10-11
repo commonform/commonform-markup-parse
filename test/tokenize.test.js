d87a0e47ee5642f69266a1a990553af8ca06151c
Kyle Mitchell <kyle@kemitchell.com>
2015-10-10 20:14:44 -0700

    Fix newline and dedent token emission

--- test/tokenize.test.js
+++ test/tokenize.test.js
@@ -6,28 +6,67 @@ tape('tokenizer', function(test) {
   test.deepEqual(
     tokenize('a test'),
     [ { type: 'text',    line: 1, column: 1, string: 'a test' },
-      { type: 'newline', line: 1, column: 7, string: '\n' } ])
+      { type: 'end',     line: 1, column: 7, string: '' } ],
+    'just text')
 
   test.deepEqual(
-    tokenize('A\n    B\nC'),
+    tokenize(
+      [ 'A',
+        '    B',
+        'C' ].join('\n')),
     [ { type: 'text',    line: 1, column: 1, string: 'A' },
-      { type: 'newline', line: 1, column: 2, string: '\n' },
       { type: 'indent',  line: 2, column: 1, string: '    ' },
       { type: 'text',    line: 2, column: 5, string: 'B' },
-      { type: 'newline', line: 2, column: 6, string: '\n' },
       { type: 'dedent',  line: 3, column: 1, string: '' },
       { type: 'text',    line: 3, column: 1, string: 'C' },
-      { type: 'newline', line: 3, column: 2, string: '\n' } ])
+      { type: 'end',     line: 3, column: 2, string: '' } ],
+    'dedent')
 
   test.deepEqual(
-    tokenize('A\n    B\n    C'),
+    tokenize(
+      [ 'A',
+        '    B',
+        '    C' ].join('\n')),
     [ { type: 'text',    line: 1, column: 1, string: 'A' },
-      { type: 'newline', line: 1, column: 2, string: '\n' },
       { type: 'indent',  line: 2, column: 1, string: '    ' },
       { type: 'text',    line: 2, column: 5, string: 'B' },
       { type: 'newline', line: 2, column: 6, string: '\n' },
       { type: 'text',    line: 3, column: 5, string: 'C' },
-      { type: 'newline', line: 3, column: 6, string: '\n' } ])
+      { type: 'dedent',  line: 3, column: 6, string: '' },
+      { type: 'end',     line: 3, column: 6, string: '' } ],
+    'consecutive at same level')
+
+  test.deepEqual(
+    tokenize(
+      [ 'A',
+        '    B',
+        '        C',
+        'D' ].join('\n')),
+    [ { type: 'text',    line: 1, column: 1, string: 'A' },
+      { type: 'indent',  line: 2, column: 1, string: '    ' },
+      { type: 'text',    line: 2, column: 5, string: 'B' },
+      { type: 'indent',  line: 3, column: 1, string: '        ' },
+      { type: 'text',    line: 3, column: 9, string: 'C' },
+      { type: 'dedent',  line: 4, column: 1, string: '' },
+      { type: 'dedent',  line: 4, column: 1, string: '' },
+      { type: 'text',    line: 4, column: 1, string: 'D' },
+      { type: 'end',     line: 4, column: 2, string: '' } ],
+    'multiple dedent')
+
+  test.deepEqual(
+    tokenize(
+      [ 'A',
+        '    B',
+        '        C' ].join('\n')),
+    [ { type: 'text',    line: 1, column: 1, string: 'A' },
+      { type: 'indent',  line: 2, column: 1, string: '    ' },
+      { type: 'text',    line: 2, column: 5, string: 'B' },
+      { type: 'indent',  line: 3, column: 1, string: '        ' },
+      { type: 'text',    line: 3, column: 9, string: 'C' },
+      { type: 'dedent',  line: 3, column: 10, string: '' },
+      { type: 'dedent',  line: 3, column: 10, string: '' },
+      { type: 'end',     line: 3, column: 10, string: '' } ],
+    'emits terminal dedent tokens')
 
   test.throws(
     function() { tokenize('\ttest') },
---
+++ times.js
@@ -0,0 +1,7 @@
+module.exports = times
+
+function times(object, times) {
+  var result = [ ]
+  for (var i = 0; i < times; i++) {
+    result.push(object) }
+  return result }
