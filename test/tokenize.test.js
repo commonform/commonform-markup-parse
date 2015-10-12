074e5c0acb5ca35a090b7100d06389065cb27d3c
Kyle Mitchell <kyle@kemitchell.com>
2015-10-11 21:18:54 -0700

    Use "END" consistently

--- test/tokenize.test.js
+++ test/tokenize.test.js
@@ -6,7 +6,7 @@ tape('tokenizer', function(test) {
   test.deepEqual(
     tokenize('a test'),
     [ { type: 'TEXT',    line: 1, column: 1, string: 'a test' },
-      { type: 'EOF',     line: 1, column: 7, string: '' } ],
+      { type: 'END',     line: 1, column: 7, string: '' } ],
     'just text')
 
   test.deepEqual(
@@ -19,7 +19,7 @@ tape('tokenizer', function(test) {
       { type: 'TEXT',    line: 2, column: 5, string: 'B' },
       { type: 'OUTDENT', line: 3, column: 1, string: '' },
       { type: 'TEXT',    line: 3, column: 1, string: 'C' },
-      { type: 'EOF',     line: 3, column: 2, string: '' } ],
+      { type: 'END',     line: 3, column: 2, string: '' } ],
     'OUTDENT')
 
   test.deepEqual(
@@ -33,7 +33,7 @@ tape('tokenizer', function(test) {
       { type: 'NEWLINE', line: 2, column: 6, string: '\n' },
       { type: 'TEXT',    line: 3, column: 5, string: 'C' },
       { type: 'OUTDENT', line: 3, column: 6, string: '' },
-      { type: 'EOF',     line: 3, column: 6, string: '' } ],
+      { type: 'END',     line: 3, column: 6, string: '' } ],
     'consecutive at same level')
 
   test.deepEqual(
@@ -50,7 +50,7 @@ tape('tokenizer', function(test) {
       { type: 'OUTDENT', line: 4, column: 1, string: '' },
       { type: 'OUTDENT', line: 4, column: 1, string: '' },
       { type: 'TEXT',    line: 4, column: 1, string: 'D' },
-      { type: 'EOF',     line: 4, column: 2, string: '' } ],
+      { type: 'END',     line: 4, column: 2, string: '' } ],
     'multiple OUTDENT')
 
   test.deepEqual(
@@ -65,7 +65,7 @@ tape('tokenizer', function(test) {
       { type: 'TEXT',    line: 3, column: 9, string: 'C' },
       { type: 'OUTDENT', line: 3, column: 10, string: '' },
       { type: 'OUTDENT', line: 3, column: 10, string: '' },
-      { type: 'EOF',     line: 3, column: 10, string: '' } ],
+      { type: 'END',     line: 3, column: 10, string: '' } ],
     'emits terminal OUTDENT tokens')
 
   test.deepEqual(
@@ -83,7 +83,7 @@ tape('tokenizer', function(test) {
       { type: 'TEXT',    line: 6, column: 9, string: 'C' },
       { type: 'OUTDENT', line: 6, column: 10, string: '' },
       { type: 'OUTDENT', line: 6, column: 10, string: '' },
-      { type: 'EOF',     line: 6, column: 10, string: '' } ],
+      { type: 'END',     line: 6, column: 10, string: '' } ],
     'ignores blank lines')
 
   test.deepEqual(
@@ -100,7 +100,7 @@ tape('tokenizer', function(test) {
       { type: 'TEXT',    line: 5, column: 9, string: 'C' },
       { type: 'OUTDENT', line: 5, column: 10, string: '' },
       { type: 'OUTDENT', line: 5, column: 10, string: '' },
-      { type: 'EOF',     line: 5, column: 10, string: '' } ],
+      { type: 'END',     line: 5, column: 10, string: '' } ],
     'ignores initial blank lines')
 
   test.throws(
