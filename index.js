561621bfbe6fa5d932db95fc147f9bb6018b1fb0
Kyle Mitchell <kyle@kemitchell.com>
2015-10-29 18:14:20 -0700

    Extract blank filling directions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Scanner = require('./scanner')
 var Parser = require('./parser').Parser
 
+var extractDirections = require('./extract-directions')
+
 module.exports = function(string) {
   var parser = new Parser
   parser.lexer = new Scanner
   var result = parser.parse(string)
-  return { form: result } }
+  return extractDirections(result) }
